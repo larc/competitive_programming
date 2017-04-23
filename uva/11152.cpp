33f5e056262dcfc3e553a7f29818cf91d5dd1858
larc <luccianorc@gmail.com>
2017-04-23 11:21:29 -0500

    add Colourful Flowers

---
+++ uva/11152.cpp
@@ -0,0 +1,24 @@
+#include <cstdio>
+#include <cmath>
+
+int main()
+{
+	double a, b, c, s, R, r, A;
+	double sunflowers, violets, roses;
+
+	while(scanf("%lf %lf %lf", &a, &b, &c) != EOF)
+	{
+		s = (a + b + c) / 2;
+		A = sqrt(s * (s - a) * (s - b) * (s - c)); // area triangle
+		R = a * b * c / 4 / A;
+		r = A / s;
+
+		sunflowers = M_PI * R * R - A;
+		roses = M_PI * r * r;
+		violets = A - roses;
+
+		printf("%.4lf %.4lf %.4lf\n", sunflowers, violets, roses);
+	}
+
+	return 0;
+}
