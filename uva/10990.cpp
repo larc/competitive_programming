56d8531903f91d72a5da02c181f7230fca221a51
larc <luccianorc@gmail.com>
2017-06-24 23:08:24 -0500

    add Another New Function

---
+++ uva/10990.cpp
@@ -0,0 +1,41 @@
+#include <cstdio>
+
+#define N 2000001
+
+int phi[N];
+int depth_phi[N];
+
+void sieve_phi()
+{
+	for(int i = 0; i < N; i++)
+		phi[i] = i;
+
+	for(int i = 2; i < N; i++)
+	{
+		if(phi[i] == i)
+		for(int n = i; n < N; n += i)
+			phi[n] = (phi[n] / i) * (i - 1);
+	}
+
+	depth_phi[1] = 0;
+	for(int i = 2; i < N; i++)
+		depth_phi[i] = 1 + depth_phi[phi[i]];
+	
+	for(int i = 2; i < N; i++)
+		depth_phi[i] += depth_phi[i - 1];
+}
+
+int main()
+{
+	sieve_phi();
+	
+	int c, m, n;
+	scanf("%d", &c);
+	while(c--)
+	{
+		scanf("%d %d", &m, &n);
+		printf("%d\n", depth_phi[n] - depth_phi[m - 1]);
+	}
+	return 0;
+}
+
