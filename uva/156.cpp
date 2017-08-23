7bafda36cf5ce7e7dd9c5a972ab3b0b91ca9c951
larc <luccianorc@gmail.com>
2017-08-22 22:19:33 -0500

    anagram and ananagram problems solutions

---
+++ uva/156.cpp
@@ -0,0 +1,58 @@
+#include <string>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+inline
+int index(const char & c)
+{
+	if(c >= 'A' && c <= 'Z')
+		return c - 'A';
+
+	return c - 'a';
+}
+
+int main()
+{
+	int compare[1000][27] = {0};
+	string words[1000];
+	string word;
+
+	int n = 0;
+	while(cin >> word && word[0] != '#')
+	{
+		for(char & c: word)
+			compare[n][index(c)]++;
+		
+		words[n++] = word;
+	}
+
+	int c, na = 0;
+	for(int i = 0; i < n; i++)
+	{
+		if(!compare[i][26])
+		for(int j = i + 1; j < n; j++)
+		{
+			c = 0;
+			for(int k = 0; k < 26; k++)
+				c += compare[i][k] == compare[j][k];
+			
+			if(c == 26)
+			{
+				compare[i][26] = 1;
+				compare[j][26] = 1;
+			}
+		}
+
+		if(!compare[i][26]) words[na++] = words[i];
+	}
+	
+	sort(words, words + na);
+
+	for(int i = 0; i < na; i++)
+		cout << words[i] << endl;
+
+	return 0;
+}
+
---
+++ uva/195.cpp
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+inline
+int letter(const char & c)
+{
+	if(c >= 'A' && c <= 'Z')
+		return c - 'A';
+	return c - 'a';
+}
+
+bool compare(const char & a, const char & b)
+{
+	if(letter(a) == letter(b))
+		return a < b;
+	return letter(a) < letter(b);
+}
+
+int main()
+{
+	int n;
+	string str;
+
+	cin >> n;
+	while(n--)
+	{
+		cin >> str;
+		sort(str.begin(), str.end(), compare);
+
+		do
+		{
+			cout << str << endl;
+		}
+		while(next_permutation(str.begin(), str.end(), compare));
+	}
+	return 0;
+}
+
