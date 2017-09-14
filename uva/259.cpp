9841aa703fd836b9b7a9778c9dec5ef009387b1c
larc <luccianorc@gmail.com>
2017-09-14 08:42:19 -0500

    solutions of software allocation and internet bandwidth

--- uva/259.cpp
+++ uva/259.cpp
@@ -1,30 +1,94 @@
 #include <cstdio>
 #include <cstring>
+#include <queue>
 
 #define N 38
+#define s 0
+#define t N - 1
 
-int G[N][N];
-int size[N];
+int G[N][N]; // adj matrix
+int path[N]; // bfs tree
+char M[N];
 
-int main()
+int augment(const int & v)
 {
-	int i, j;
-
-	char line[16];
-	while(fgets(line, 16, stdin))
+	if(path[v] >= 0 && v != path[v])
 	{
-		memset(size, 0, sizeof(N));
-		
-		do
-		{
-			i = line[0] - 'A' + 1;
-			for(j = 3; line[j] != ';'; j++)
-				G[i][size[i]++] = j - '0' + 27;
-		}
-		while(fgets(line, 16, stdin) && line[0] != '\n');
-
-		printf("!\n");
+		augment(path[v]);
+		G[path[v]][v] -= 1;
+		G[v][path[v]] += 1;
+		M[v] = path[v] - 1 + 'A';
+		return 1;
 	}
 
 	return 0;
 }
+
+int bfs()
+{
+	memset(path, -1, sizeof(path));
+	std::queue<int> q;
+	q.push(s);
+	path[s] = 0;
+	
+	int u, v;
+	while(!q.empty())
+	{
+		u = q.front();
+		q.pop();
+		
+		if(u == t) break;
+		
+		for(v = 0; v < N; v++)
+		{
+			if(G[u][v] > 0 && path[v] == -1)
+			{
+				path[v] = u;
+				q.push(v);
+			}
+		}
+	}
+
+	return augment(t);
+}
+
+int main()
+{
+	int i, j, mf;
+
+	char line[16];
+	while(fgets(line, 16, stdin))
+	{
+		memset(G, 0, sizeof(G));
+		memset(M, '_', sizeof(M));
+		
+		do
+		{
+			i = line[0] - 'A' + 1;
+			G[s][i] = line[1] - '0';
+			for(j = 3; line[j] != ';'; j++)
+				G[i][line[j] - '0' + 27] = 1;
+		}
+		while(fgets(line, 16, stdin) && line[0] != '\n');
+
+		for(i = 27; i < t; i++)
+			G[i][t] = 1;
+		
+		while(bfs());
+		
+		mf = 0;
+		for(i = 1; i < 27; i++)
+			mf += G[s][i];
+
+		if(mf) printf("!\n");
+		else
+		{
+			for(i = 27; i < t; i++)
+				putchar(M[i]);
+			printf("\n");
+		}
+	}
+
+	return 0;
+}
+
--- uva/820.cpp
+++ uva/820.cpp
@@ -1,33 +1,88 @@
 #include <cstdio>
 #include <cstring>
+#include <cmath>
+#include <queue>
 
 #define N 101
+#define INF 1 << 31 - 1
 
-int W[N][N];
-int G[N][N];
-int size[N];
+int W[N][N]; // edges weight 
+int G[N][N]; // adj list
+int size[N]; 
+int path[N]; // bfs tree
+
+int augment(const int & v, int min_edge)
+{
+	if(path[v] > 0)
+	{
+		min_edge = augment(path[v], fmin(min_edge, W[path[v]][v]));
+		W[path[v]][v] -= min_edge;
+		W[v][path[v]] += min_edge;
+	}
+
+	return min_edge;
+}
+
+int bfs(const int & s, const int & t)
+{
+	memset(path, -1, sizeof(path));
+	std::queue<int> q;
+	q.push(s);
+	path[s] = 0;
+
+	int u, v;
+	while(!q.empty())
+	{
+		u = q.front();
+		q.pop();
+
+		if(u == t) break;
+
+		for(int i = 0; i < size[u]; i++)
+		{
+			v = G[u][i];
+			if(W[u][v] > 0 && path[v] == -1)
+			{
+				path[v] = u;
+				q.push(v);
+			}
+		}
+	}
+	
+	return augment(t, INF);
+}
 
 int main()
 {
 	int n, s, t, c;
-	int u, v, b;
+	int u, v, b, mf, f;
 
 	int net = 1;
 	while(scanf("%d", &n), n)
 	{
 		memset(size, 0, sizeof(size));
+		memset(W, 0, sizeof(W));
 
 		scanf("%d %d %d", &s, &t, &c);
 		while(c--)
 		{
 			scanf("%d %d %d", &u, &v, &b);
-			G[u][size[u]++] = v;
-			G[v][size[v]++] = u;
-			W[u][v] = W[v][u] = b;
-		}	
+			if(!W[u][v])
+			{
+				G[u][size[u]++] = v;
+				G[v][size[v]++] = u;
+			}
+
+			W[u][v] += b;
+			W[v][u] += b;
+		}
+
+		mf = 0;
+		while((f = bfs(s, t)) < INF)
+			mf += f;
 		
 		printf("Network %d\n", net++);
-		printf("The bandwidth %d.\n\n", 25);
+		printf("The bandwidth is %d.\n\n", mf);
 	}
 
 	return 0;
