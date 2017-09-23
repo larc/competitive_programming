974ccb4b563dc8d7363b617745823de0b07fed07
larc <luccianorc@gmail.com>
2017-09-23 16:44:42 -0500

    add solutions to 558 wormholes (negative cycle) and 10054 the necklace (print euler tour)

---
+++ uva/10054.cpp
@@ -0,0 +1,61 @@
+#include <cstdio>
+#include <cstring>
+
+#define N 51
+
+int G[N][N];
+int size[N];
+
+inline bool is_eulerian()
+{
+	for(int i = 0; i < N; i++)
+		if(size[i] & 1) return 0;
+	
+	return 1;
+}
+
+void dfs(const int & u)
+{
+	for(int v = 0; v < N; v++)
+	{
+		if(G[u][v])
+		{
+			G[u][v]--;
+			G[v][u]--;
+			dfs(v);
+			printf("%d %d\n", v, u);
+		}
+	}
+}
+
+int main()
+{
+	int t, n, u, v;
+	
+	int c = 0;
+	scanf("%d", &t);
+	while(t--)
+	{
+		memset(G, 0, sizeof(G));
+		memset(size, 0, sizeof(size));
+
+		scanf("%d", &n);
+		while(n--)
+		{
+			scanf("%d %d", &u, &v);
+			G[u][v]++;
+			G[v][u]++;
+			size[u]++;
+			size[v]++;
+		}
+		
+		printf("Case #%d\n", ++c);
+		if(!is_eulerian()) printf("some beads may be lost\n");
+		else dfs(u);
+		
+		if(t) printf("\n");
+	}
+
+	return 0;
+}
+
---
+++ uva/558.cpp
@@ -0,0 +1,56 @@
+#include <cstdio>
+#include <cstring>
+
+#define N 1000
+#define M 2000
+
+struct edge_t
+{
+	int u, v, t;
+};
+
+edge_t edges[M];
+int dist[N];
+
+bool relax_edges(const int & m)
+{
+	int d;
+	bool relax = false;
+	for(int e = 0; e < m; e++)
+	{
+		edge_t & edge = edges[e];
+
+		d = dist[edge.u] + edge.t;
+		if(d < dist[edge.v])
+		{
+			dist[edge.v] = d;
+			relax = true;
+		}
+	}
+
+	return relax;
+}
+
+int main()
+{
+	int c, n, m;
+
+	scanf("%d", &c);
+	while(c--)
+	{
+		scanf("%d %d", &n, &m);
+		for(int e = 0; e < m; e++)
+			scanf("%d %d %d", &edges[e].u, &edges[e].v, &edges[e].t);
+
+		// Bellman Ford's algorithm
+		memset(dist, 0, sizeof(dist));
+
+		while(--n)
+			relax_edges(m);
+
+		if(relax_edges(m)) printf("possible\n");
+		else printf("not possible\n");
+	}
+	return 0;
+}
+
