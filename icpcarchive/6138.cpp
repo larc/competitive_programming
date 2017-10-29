98a5409774bc3ceb7cac6fe9d94e97d6d2bdbab7
larc <luccianorc@gmail.com>
2017-10-28 22:30:47 -0500

    add Hours and Minutes

---
+++ icpcarchive/6138.cpp
@@ -0,0 +1,16 @@
+// 6138 - Hours and Minutes
+
+#include <cstdio>
+
+int main()
+{
+	int a;
+	while(scanf("%d", &a) != EOF)
+	{
+		if(a % 6) printf("N\n");
+		else printf("Y\n");
+	}
+
+	return 0;
+}
+
---
+++ uva/12531.cpp
@@ -0,0 +1,16 @@
+// 12531 - Hours and Minutes
+
+#include <cstdio>
+
+int main()
+{
+	int a;
+	while(scanf("%d", &a) != EOF)
+	{
+		if(a % 6) printf("N\n");
+		else printf("Y\n");
+	}
+
+	return 0;
+}
+
