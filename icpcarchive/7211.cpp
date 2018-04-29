ee850c225f7d2b6ecfd28b29807ca337027dda76
Luciano Arnaldo Romero Calla <luccianorc@gmail.com>
2018-04-29 10:36:39 -0500

    add identifying tea solution

---
+++ icpcarchive/7211.cpp
@@ -0,0 +1,23 @@
+// 7211 - Identifying tea
+
+#include <cstdio>
+
+int main()
+{
+	int t, r, a;
+
+	while(scanf("%d", &t) != EOF)
+	{
+		r = 0;
+		for(int i = 0; i < 5; i++)
+		{
+			scanf("%d", &a);
+			r += a == t;
+		}
+
+		printf("%d\n", r);
+	}
+
+	return 0;
+}
+
