d8ca0c3bf58c21e1fcb6b8d6d475f103056d5562
Luciano Arnaldo Romero Calla <luccianorc@gmail.com>
2018-05-01 15:09:19 -0500

    add breaking board and factorial products solutions

---
+++ uva/12705.cpp
@@ -0,0 +1,57 @@
+// 12705 - Breaking Board
+
+#include <cstdio>
+#include <map>
+#include <queue>
+
+using namespace std;
+
+int cost[36] = {	2,
+					3, 3,
+					4, 4, 4,
+					5, 5, 5, 5,
+					6, 6, 6, 6, 6,
+					7, 7, 7, 7, 7, 7,
+					8, 8, 8, 8, 8,
+					9, 9, 9, 9,
+					10, 10, 10,
+					11, 11,
+					12 };
+
+int main()
+{
+	int n, min_cost;
+	char str[128];
+	char * c;
+
+	scanf("%d", &n);
+	fgets(str, 128, stdin);	
+	while(n--)
+	{
+		fgets(str, 128, stdin);
+		map<char, int> freq;
+		priority_queue<pair<int, char> > q;
+
+		c = str;
+		while(*c != '\n')
+		{
+			if(*c != ' ') freq[*c]++;
+			c++;
+		}
+		
+		for(auto & p: freq)
+			q.push({p.second, p.first});
+		
+		min_cost = 0;
+		for(int i = 0; !q.empty(); i++)
+		{
+			min_cost += q.top().first * cost[i];
+			q.pop();
+		}
+
+		printf("%d\n", min_cost);
+	}
+
+	return 0;
+}
+
---
+++ uva/12765.cpp
@@ -0,0 +1,59 @@
+// 12765 - Factorial Products
+
+#include <cstdio>
+#include <cstring>
+
+int counter[10];
+bool check()
+{
+	if(counter[2]) return 0;
+	if(counter[3]) return 0;
+	if(counter[5]) return 0;
+	if(counter[7]) return 0;
+
+	return 1;
+}
+
+int main()
+{
+	int n, m, p;
+	
+	while(scanf("%d %d", &n, &m), n && m)
+	{
+		memset(counter, 0, sizeof(counter));
+
+		while(n--)
+		{
+			scanf("%d", &p);
+			for(int i = 2; i <= p; i++)
+				switch(i)
+				{
+					case 4: counter[2] += 2; break;
+					case 6: counter[2]++; counter[3]++; break;
+					case 8: counter[2] += 3; break;
+					case 9: counter[3] += 2; break;
+					default: counter[i]++;
+				}
+		}
+
+		while(m--)
+		{
+			scanf("%d", &p);
+			for(int i = 2; i <= p; i++)
+				switch(i)
+				{
+					case 4: counter[2] -= 2; break;
+					case 6: counter[2]--; counter[3]--; break;
+					case 8: counter[2] -= 3; break;
+					case 9: counter[3] -= 2; break;
+					default: counter[i]--;
+				}
+		}
+		
+		if(check()) printf("YES\n");
+		else printf("NO\n");
+	}
+
+	return 0;
+}
+
