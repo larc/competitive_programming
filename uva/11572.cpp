ccb974b903a60e6747e94c3d9b71b66d8efcad79
Luciano Arnaldo Romero Calla <luccianorc@gmail.com>
2018-05-23 00:57:52 -0500

    add Lemmings Battle, Unique Snowflaques (map, priority queue problems).

---
+++ uva/11572.cpp
@@ -0,0 +1,43 @@
+// 11572 - Unique Snowflakes
+
+#include <cstdio>
+#include <map>
+
+#define N 1000000
+
+int main()
+{
+	std::map<int, int> last_idx;
+
+	int n_cases, n;
+	int m, k, idx, max, sf;
+
+	scanf("%d", &n_cases);
+	while(n_cases--)
+	{
+		max = m = 0;
+		idx = 0;
+
+		scanf("%d", &n);
+		for(int i = 1; i <= n; i++)
+		{
+			scanf("%d", &sf);
+
+			if(k = last_idx[sf])
+			{
+				if(k > idx) idx = k;
+				m = i - idx;
+			}
+			else m++;
+			
+			last_idx[sf] = i;
+			if(m > max) max = m;
+		}
+
+		printf("%d\n", max);
+		last_idx.clear();
+	}
+	
+	return 0;
+}
+
---
+++ uva/978.cpp
@@ -0,0 +1,63 @@
+// 978 - Lemmings Battle!
+
+#include <cstdio>
+#include <queue>
+
+int main()
+{
+	int winners[100000];
+	int n, f, g, b, p, i;
+
+	scanf("%d", &n);
+	while(n--)
+	{
+		std::priority_queue<int> soldiers[2];
+
+		scanf("%d %d %d", &f, &g, &b);
+
+		while(g--)
+		{
+			scanf("%d", &p);
+			soldiers[0].push(p);
+		}
+		while(b--)
+		{
+			scanf("%d", &p);
+			soldiers[1].push(p);
+		}
+		
+		while(!soldiers[0].empty() && !soldiers[1].empty())
+		{
+			for(i = 0; i < f && !soldiers[0].empty() && !soldiers[1].empty(); i++)
+			{
+				winners[i] = soldiers[0].top() - soldiers[1].top();
+				soldiers[0].pop(); soldiers[1].pop();
+			}
+			
+			while(i--)
+				if(winners[i] > 0) soldiers[0].push(winners[i]);
+				else if(winners[i] < 0) soldiers[1].push(-winners[i]);
+		}
+		
+		if(soldiers[0].empty() && soldiers[1].empty())
+			printf("green and blue died\n");
+		else
+		{
+			p = !soldiers[1].empty() && soldiers[0].empty();
+
+			if(p) printf("blue wins\n");
+			else printf("green wins\n");
+
+			while(!soldiers[p].empty())
+			{
+				printf("%d\n", soldiers[p].top());
+				soldiers[p].pop();
+			}
+		}
+
+		if(n) putchar('\n');
+	}
+
+	return 0;
+}
+
