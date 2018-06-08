d5f415397baa324f3c7d00cd9d3fccbc5d138f8b
Luciano Arnaldo Romero Calla <luccianorc@gmail.com>
2018-06-07 21:21:25 -0500

    add color the fence and cd and pwd commands codeforces

---
+++ codeforces/158C.cpp
@@ -0,0 +1,48 @@
+#include <cstdio>
+
+#define N 201
+
+int main()
+{
+	char pwd[N * 50], cmd[4], path[N];
+	
+	int n, idx, i;
+	scanf("%d", &n);
+	
+	pwd[idx = 0] = 0;
+	while(n--)
+	{
+		scanf("%s", cmd);
+		if(cmd[0] == 'c') // cd
+		{
+			scanf("%s", path);
+
+			i = 0;
+			if(path[i] == '/') { idx = 0; i++; }
+			while(path[i])
+			{
+				if(path[i] == '.' && path[i + 1] == '.')
+				{
+					i += 2;
+					idx -= 2;
+					while(idx >= 0 && pwd[idx] != '/') idx--;
+					idx++;
+				}
+				else 
+				{
+					while(path[i] && path[i] != '/')
+						pwd[idx++] = path[i++];
+				
+					pwd[idx++] = '/';
+				}
+				
+				if(path[i] == '/') i++;
+				pwd[idx] = 0;
+			}
+		}
+		else printf("/%s\n", pwd);
+	}
+
+	return 0;
+}
+
---
+++ codeforces/349B.cpp
@@ -0,0 +1,51 @@
+#include <cstdio>
+
+int main()
+{
+	int v, d, first;
+	int num[10], min;
+	
+	scanf("%d", &v);
+
+	min = 0;
+	num[0] = 1000000;
+	for(int i = 1; i < 10; i++)
+	{
+		scanf("%d", num + i);
+		if(num[i] <= num[min]) min = i;
+	}
+
+	if(v < num[min])
+	{
+		printf("-1\n");
+		return 0;
+	}
+
+	d = v / num[min];
+	v %= num[min];
+
+	first = 0;
+	while(d && v && !first)
+	{
+		for(int i = 1; i < 10; i++)
+		{
+			if(v + num[min] >= num[i])
+				first = i;
+		}
+
+		if(first)
+		{
+			d--;
+			putchar(first + '0');
+			v = v + num[min] - num[first];
+			first = 0;
+		}
+		else first = 1;
+	}
+	
+	while(d--) putchar(min + '0');
+	putchar('\n');
+
+	return 0;
+}
+
