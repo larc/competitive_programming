f1be57ea3f8b9660f32f6fd17b224a9fcdf5626f
Luciano Arnaldo Romero Calla <luccianorc@gmail.com>
2018-06-10 18:57:20 -0500

    add uva Bombs! NO they are Mines!! and Zones

---
+++ uva/1047.cpp
@@ -0,0 +1,97 @@
+// 1047 - Zones
+
+#include <cstdio>
+#include <cstring>
+
+#define N (1 << 20)
+
+int ones(int s)
+{
+	int count = 0;
+
+	while(s)
+	{
+		count++;
+		s -= s & (-s);
+	}
+
+	return count;
+}
+
+int main()
+{
+	int * pow_set = new int[N];
+	int common[10][2];
+	int n, b, m, idx, k, j;
+
+	int n_cases = 1;
+	while(scanf("%d %d", &n, &b), n)
+	{
+		for(int i = 0; i < n; i++)
+			scanf("%d", &pow_set[1 << i]);
+
+		scanf("%d", &m);
+		for(int i = 0; i < m; i++)
+		{
+			idx = 0;
+
+			scanf("%d", &k);
+			while(k--) 
+			{
+				scanf("%d", &j);
+				idx |= 1 << (j - 1);
+			}
+
+			common[i][0] = idx;
+			scanf("%d", &common[i][1]);
+		}
+
+		n = 1 << n;
+		k = (1 << b) - 1;
+		for(int i = k; i < n; i++)
+		{
+			if(ones(i) == b && b > 1)
+			{
+				pow_set[i] = 0;
+
+				idx = i;
+				while(idx)
+				{
+					pow_set[i] += pow_set[idx & (-idx)];
+					idx -= idx & (-idx);
+				}
+				
+				j = 0;
+				while(j < m)
+				{
+					if(ones(i & common[j][0]))
+						pow_set[i] -= (ones(i & common[j][0]) - 1) * common[j][1];
+
+					j++;
+				}
+
+			}
+			if(ones(i) == b && pow_set[i] > pow_set[k]) k = i;
+		}
+		
+
+		printf("Case Number  %d\n", n_cases++);
+		printf("Number of Customers: %d\n", pow_set[k]);
+		printf("Locations recommended:");
+
+		j = 1;
+		while(k)
+		{
+			if(k & 1) printf(" %d", j);
+			k >>= 1;
+			j++;
+		}
+
+		printf("\n\n");
+	}
+	
+	delete [] pow_set;
+
+	return 0;
+}
+
---
+++ uva/10653.cpp
@@ -0,0 +1,82 @@
+// 10653 - Bombs! NO they are Mines!!
+
+#include <cstdio>
+#include <cstring>
+#include <queue>
+
+#define N 1000
+
+struct point
+{
+	int x, y;
+};
+
+point moves[4] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+int grid[N][N];
+int R, C;
+
+bool valid(const point & p)
+{
+	if(p.x < 0 || p.x >= R) return 0;
+	if(p.y < 0 || p.y >= C) return 0;
+	return 1;
+}
+
+const int & bfs(point s, const point & d)
+{
+	std::queue<point> q;
+	q.push(s);
+
+	grid[s.x][s.y] = 0;
+
+	point p;
+	while(!q.empty())
+	{
+		s = q.front();
+		q.pop();
+
+		if(s.x == d.x && s.y == d.y) break;
+
+		for(int i = 0; i < 4; i++)
+		{
+			p.x = s.x + moves[i].x;
+			p.y = s.y + moves[i].y;
+			if(valid(p) && grid[p.x][p.y] < 0)
+			{
+				grid[p.x][p.y] = grid[s.x][s.y] + 1;
+				q.push(p);
+			}
+		}
+	}
+
+	return grid[d.x][d.y];
+}
+
+int main()
+{
+	int n, r, m, c;
+	point s, d;
+
+	while(scanf("%d %d", &R, &C), R && C)
+	{
+		memset(grid, -1, sizeof(grid));
+		
+		scanf("%d", &n);
+		while(n--)
+		{
+			scanf("%d %d", &r, &m);
+			while(m--)
+			{
+				scanf("%d", &c);
+				grid[r][c] = N;
+			}
+		}
+
+		scanf("%d %d", &s.x, &s.y);
+		scanf("%d %d", &d.x, &d.y);
+		printf("%d\n", bfs(s, d));
+	}
+
+	return 0;
+}
+
