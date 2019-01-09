2d44d0da21287c956051fb8c073b80f86dd65fcd
Luciano Arnaldo Romero Calla <luccianorc@gmail.com>
2019-01-09 12:07:23 -0500

    uva Babel (dijkstra), Knight in a War Grid (dfs), and  Forwarding Emails (dfs)

---
+++ uva/11906.cpp
@@ -0,0 +1,63 @@
+// 11906 - Knight in a War Grid
+
+#include <cstdio>
+#include <cstring>
+
+#define N 100
+
+int grid[N][N], r, c, m, n;
+
+void dfs(const int & x, const int & y)
+{
+	if(x < 0 || y < 0) return;
+	if(x >= r || y >= c) return;
+	if(grid[x][y] == -1) return;
+	if(grid[x][y]++) return;
+
+	dfs(x + n, y + m);
+	dfs(x - n, y + m);
+	dfs(x + n, y - m);
+	dfs(x - n, y - m);
+	
+	dfs(x + m, y + n);
+	dfs(x - m, y + n);
+	dfs(x + m, y - n);
+	dfs(x - m, y - n);
+}
+
+int main()
+{
+	int n_cases, w, x, y;
+
+	scanf("%d", &n_cases);
+	for(int ic = 1; ic <= n_cases; ic++)
+	{
+		scanf("%d %d %d %d", &r, &c, &m, &n);
+		memset(grid, 0, sizeof(grid));
+
+		scanf("%d", &w);
+		while(w--)
+		{
+			scanf("%d %d", &x, &y);
+			grid[x][y] = -1;
+		}
+		
+		dfs(0, 0); grid[0][0]--;
+
+		x = y = 0;
+		for(int i = 0; i < r; i++)
+		for(int j = 0; j < c; j++)
+			if(grid[i][j] > 0)
+			{
+				if(!m || !n || m == n) grid[i][j] >>= 1;
+				x += !(grid[i][j] & 1);
+				y += grid[i][j] & 1;
+			}
+		
+		if(!x && !y) x = 1;
+		printf("Case %d: %d %d\n", ic, x, y);
+	}
+
+	return 0;
+}
+
---
+++ uva/12442.cpp
@@ -0,0 +1,55 @@
+// 12442 - Forwarding Emails
+
+#include <cstdio>
+#include <cstring>
+
+#define N 50001
+
+bool visited[N];
+int send[N], path[N];
+
+int dfs(const int & u)
+{
+	if(visited[u]) return 0;
+
+	visited[u] = 1;
+	path[u] = 1 + dfs(send[u]);
+	visited[u] = 0;
+
+	return path[u];
+}
+
+int main()
+{
+	int n_cases, n, u, v;
+
+	scanf("%d", &n_cases);
+	for(int c = 1; c <= n_cases; c++)
+	{
+		memset(path, 0, sizeof(path));
+		memset(visited, 0, sizeof(visited));
+
+		scanf("%d", &n);
+		for(int i = 0; i < n; i++)
+		{
+			scanf("%d %d", &u, &v);
+			send[u] = v;
+		}
+		
+		u = 0; v = 0;
+
+		for(int i = 1; i <= n; i++)
+		{
+			if(!path[i] && dfs(i) > v)
+			{
+				v = path[i];
+				u = i;
+			}
+		}
+
+		printf("Case %d: %d\n", c, u);
+	}
+
+	return 0;
+}
+
