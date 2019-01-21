d42b93b0bc01c33d569dc531529715b59db72d4f
Luciano Arnaldo Romero Calla <luccianorc@gmail.com>
2019-01-21 14:16:00 -0500

    uva 10938 Flea circus (LCA), uva 10807 Prim (graphs complete search)

---
+++ uva/10807.cpp
@@ -0,0 +1,109 @@
+// 10807 - Prim
+
+#include <cstdio>
+#include <algorithm>
+
+#define N 11
+#define M 25
+
+struct union_find
+{
+	int set[N];
+	
+	void init(const int & n)
+	{
+		for(int i = 1; i <= n; i++)
+			set[i] = i;
+	}
+
+	int find(const int & x)
+	{
+		if(set[x] == x) return x;
+		return set[x] = find(set[x]);
+	}
+
+	bool join(int x, int y)
+	{
+		x = find(x);
+		y = find(y);
+
+		if(x == y) return 0;
+		
+		set[y] = x;
+		return 1;
+	}
+};
+
+struct edge
+{
+	int u, v, w;
+};
+
+bool operator < (const edge & a, const edge & b)
+{
+	return a.w < b.w;
+}
+
+int n, m, max_cost;
+edge G[M];	// graph list of edges
+
+void dfs(const int & i, const int & na, const int & nb, 
+						const int & ca, const int & cb,
+						const union_find & ufa, const union_find & ufb)
+{
+	if(ca + cb >= max_cost) return;
+
+	if(na == n - 1 && nb == n - 1)
+	{
+		max_cost = ca + cb;
+		return;
+	}
+	
+	if(ca + cb + (n - 1 - na + n - 1 - nb) * G[i].w >= max_cost) return;
+	if(n - 1 - na + n - 1 - nb > m - i) return;
+	
+	union_find uf;
+	if(na < n - 1)
+	{
+		uf = ufa;
+		if(uf.join(G[i].u, G[i].v))
+			dfs(i + 1, na + 1, nb, ca + G[i].w, cb, uf, ufb);
+		else dfs(i + 1, na, nb, ca, cb, ufa, ufb);
+	}
+	
+	uf = ufb;
+	if(uf.join(G[i].u, G[i].v))
+		dfs(i + 1, na, nb + 1, ca, cb + G[i].w, ufa, uf);
+	else dfs(i + 1, na, nb, ca, cb, ufa, ufb);
+}
+
+int main()
+{
+	union_find ufa, ufb;
+	
+	int inf;
+	while(scanf("%d", &n) && n)
+	{
+		scanf("%d", &m);
+
+		max_cost = 1;
+		for(int i = 0; i < m; i++)
+		{
+			scanf("%d %d %d", &G[i].u, &G[i].v, &G[i].w);
+			max_cost += G[i].w;
+		}
+
+		std::sort(G, G + m);
+		
+		inf = max_cost;
+		ufa.init(n);
+		ufb.init(n);
+		dfs(0, 0, 0, 0, 0, ufa, ufb);
+
+		if(max_cost < inf) printf("%d\n", max_cost);
+		else printf("No way!\n");
+	}
+
+	return 0;
+}
+
---
+++ uva/10938.cpp
@@ -0,0 +1,108 @@
+// 10938 - Flea circus
+
+#include <cstdio>
+#include <cmath>
+#include <vector>
+
+#define N 100000
+#define M 18
+
+using namespace std;
+
+vector<int> G[N];
+int parent[N];
+int level[N];
+
+int idx[N], euler[N << 1];	// euler tour
+int st[M][N << 1];			// sparse table
+
+void dfs(const int & u, int & m)
+{
+	idx[u] = m;
+	euler[m++] = u;
+
+	for(const int & v: G[u]) if(parent[u] != v)
+	{
+		level[v] = level[u] + 1;
+		parent[v] = u;
+
+		dfs(v, m);
+		euler[m++] = u;
+	}
+}
+
+void init_st(const int & m)
+{
+	for(int i = 0; i < m; i++)
+		st[0][i] = i;
+	
+	int k;
+	for(int i = 1; (1 << i) <= m; i++)
+	{
+		k = 1 << (i - 1);
+		for(int j = 0; j + (1 << i) < m; j++)
+		{
+			const int & u = st[i - 1][j];
+			const int & v = st[i - 1][j + k];
+			st[i][j] = level[euler[u]] < level[euler[v]] ? u : v;
+		}
+	}
+}
+
+int lca(const int & a, const int & b)
+{
+	if(b < a) return lca(b, a);
+	
+	int k = log2(b - a + 1);
+
+	const int & i = st[k][a];
+	const int & j = st[k][b - (1 << k) + 1];
+
+	return level[euler[i]] < level[euler[j]] ? euler[i] : euler[j];	
+}
+
+int main()
+{
+	int n, m, q, u, v, dist;
+
+	level[1] = parent[1] = 0;
+	while(scanf("%d", &n) && n)
+	{
+		for(int i = 1; i < n; i++)
+		{
+			scanf("%d %d", &u, &v);
+
+			G[u].push_back(v);
+			G[v].push_back(u);
+		}
+		
+		dfs(1, m = 0);
+		init_st(m);
+
+		scanf("%d", &q);
+		while(q--)
+		{
+			scanf("%d %d", &u, &v);
+			
+			dist = level[u] + level[v] - (level[lca(idx[u], idx[v])] << 1);
+			u = level[u] > level[v] ? u : v;
+			v = dist >> 1;
+			
+			while(v--) u = parent[u];
+			
+			if(dist & 1)
+			{
+				v = parent[u];
+				if(u > v) printf("The fleas jump forever between %d and %d.\n", v, u);
+				else printf("The fleas jump forever between %d and %d.\n", u, v);
+			}
+			else printf("The fleas meet at %d.\n", u);
+		}
+
+		for(int i = 1; i <= n; i++)
+			G[i].clear();
+	}
+
+	return 0;
+}
+
