701dd5e944bc35b41c63a5c81f56c3f1f13d3f95
Luciano Arnaldo Romero Calla <luccianorc@gmail.com>
2019-02-10 20:25:08 -0500

    codeforces round 538 div 2, problems A and B

---
+++ codeforces/1114A.cpp
@@ -0,0 +1,35 @@
+#include <cstdio>
+
+int main()
+{
+	int x, y, z;
+	int a, b, c;
+
+	scanf("%d %d %d", &x, &y, &z);
+	scanf("%d %d %d", &a, &b, &c);
+	
+	if(a < x)
+	{
+		printf("NO\n");
+		return 0;
+	}
+
+	a -= x; a += b;
+	if(a < y)
+	{
+		printf("NO\n");
+		return 0;
+	}
+
+	a -= y; a += c;
+	if(a < z)
+	{
+		printf("NO\n");
+		return 0;
+	}
+
+	printf("YES\n");
+
+	return 0;
+}
+
---
+++ codeforces/1114B.cpp
@@ -0,0 +1,47 @@
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	int n, m, k, a;
+	
+	scanf("%d %d %d", &n, &m, &k);
+	
+	vector<pair<int, int> > A(n);
+	for(int i = 0; i < n; i++)
+	{
+		scanf("%d", &a);
+		A[i] = {a, i};
+	}
+	
+	sort(A.begin(), A.end());
+	bool V[n] = {};
+	
+	long long sum = 0;
+	int p = m * k;
+	for(int i = n - 1; i >= 0 && p--; i--)
+	{
+		sum += A[i].first;
+		V[A[i].second] = 1;
+	}
+	
+	printf("%lld\n", sum);
+
+	p = 0; k--;
+	for(int i = 0; i < n && k; i++)
+	{
+		p += V[i];
+		if(V[i] && p % m == 0)
+		{
+			printf("%d ", i + 1);
+			k--;
+		}
+	}
+	
+	putchar('\n');
+	return 0;
+}
+
