5c6a287feac8ee647821fb0c9d6eb1b5174411e0
Luciano Arnaldo Romero Calla <luccianorc@gmail.com>
2019-03-02 22:13:52 -0500

    uva/LA What Base Is This?, Sum of Consecutive Prime Numbers

---
+++ uva/1210.cpp
@@ -0,0 +1,52 @@
+// 1210 - Sum of Consecutive Prime Numbers
+
+#include <cstdio>
+
+#define N 10001
+
+int lower_prime[N], primes[N], np;
+
+void sieve()
+{
+	bool not_prime[N] = {};
+
+	np = 0; primes[np++] = 2;
+	for(long long i = 3; i < N; i += 2)
+		if(!not_prime[i])
+		{
+			primes[np++] = i;
+			for(long long j = i * i; j < N; j += i << 1)
+				not_prime[j] = 1;
+		}
+	
+	primes[np] = N;
+	for(int i = 0; i < np; i++)
+	for(int j = primes[i]; j < primes[i + 1]; j++)
+		lower_prime[j] = i;
+}
+
+int main()
+{
+	sieve();
+
+	int n, count, sum, i;
+	while(scanf("%d", &n) && n)
+	{
+		i = lower_prime[n];
+		count = sum = 0;
+
+		for(int j = i; j >= 0; j--)
+		{
+			sum += primes[j];
+			count += sum == n;
+
+			if(sum >= n)
+				sum -= primes[i--];
+		}
+
+		printf("%d\n", count);
+	}
+
+	return 0;
+}
+
---
+++ uva/343.cpp
@@ -0,0 +1,69 @@
+// 343 - What Base Is This?
+
+#include <cstdio>
+
+#define N 36
+
+int value(const char & c)
+{
+	if('0' <= c && c <= '9')
+		return c - '0';
+	
+	return c - 'A' + 10;
+}
+
+int to_base_10(char * str, const int & b)
+{
+	int n = 0;
+
+	while(*str)
+	{
+		n *= b;
+		n += value(*str++);
+	}
+
+	return n;
+}
+
+int min_base(char * str)
+{
+	int b = 2;
+
+	while(*str)
+	{
+		if(value(*str) >= b)
+			b = value(*str) + 1;
+		str++;
+	}
+
+	return b;
+}
+
+int main()
+{
+	char X[N], Y[N];
+	int n, ix, iy, i, j;
+
+	while(scanf("%s %s", X, Y) != EOF)
+	{
+		ix = min_base(X);
+		iy = min_base(Y);
+
+		for(i = ix; i <= N; i++)
+		{
+			n = to_base_10(X, i);
+
+			for(j = iy; j <= N; j++)
+				if(n == to_base_10(Y, j))
+					break;
+
+			if(j <= N) break;
+		}
+
+		if(i > N) printf("%s is not equal to %s in any base 2..36\n", X, Y);
+		else printf("%s (base %d) = %s (base %d)\n", X, i, Y, j);
+	}
+
+	return 0;
+}
+
