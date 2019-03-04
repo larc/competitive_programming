803a6a35a2b258fccaee1e3206649761eb1500fa
Luciano Arnaldo Romero Calla <luccianorc@gmail.com>
2019-03-04 18:03:32 -0500

    uva Soya Milk (area, volume, triangle)

---
+++ uva/11909.cpp
@@ -0,0 +1,22 @@
+// 11909 - Soya Milk
+
+#include <cstdio>
+#include <cmath>
+
+int main()
+{
+	double l, w, h, theta, hh;
+	while(scanf("%lf %lf %lf %lf", &l, &w, &h, &theta) != EOF)
+	{
+		theta *= M_PI / 180;
+		hh = l * tan(theta);
+
+		if(hh > h) 
+			printf("%.3lf mL\n", w * h * h * tan(M_PI / 2 - theta) / 2);
+		else 
+			printf("%.3lf mL\n", l * w * ( h - hh / 2));
+	}
+
+	return 0;
+}
+
