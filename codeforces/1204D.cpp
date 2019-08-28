f018e9bb9e4e265f49dc6c2a7ca65c68f7d4407d
Luciano Arnaldo Romero Calla <luccianorc@gmail.com>
2019-08-28 09:46:24 +0200

    codeforces round #581 div2, D

---
+++ codeforces/1204D.cpp
@@ -0,0 +1,24 @@
+#include <cstdio>
+
+#define N 100001
+
+int main()
+{
+	char str[N];
+	scanf("%s", str);
+	
+	int n = 0;
+	while(str[++n]);
+	
+	int zeros = 0;
+	while(n--)
+		if(str[n] == '1')
+			if(!zeros) str[n] = '0';
+			else zeros--;
+		else zeros++;
+		
+	printf("%s\n", str);
+
+	return 0;
+}
+
