f81f66691822397ecc0fff90195fb908b061c4a2
Luciano Arnaldo Romero Calla <luccianorc@gmail.com>
2020-02-18 12:28:38 +0100

    hascode practice round 2020

---
+++ google_hashcode/2020/practice.cpp
@@ -0,0 +1,34 @@
+#include <cstdio>
+#include <stack>
+
+#define N 100000
+
+int main()
+{
+	int m, n;
+	int S[N];
+
+	std::stack<int> order;
+
+	scanf("%d %d", &m, &n);
+	for(int i = 0; i < n; i++)
+		scanf("%d", S + i);
+
+	for(int i = n - 1; i >= 0; i--)
+		if(m - S[i] >= 0)
+		{
+			m -= S[i];
+			order.push(i);
+		}
+	
+	printf("%lu\n", order.size());
+	while(!order.empty())
+	{
+		printf("%d ", order.top());
+		order.pop();
+	}
+	putchar('\n');
+
+	return 0;
+}
+
