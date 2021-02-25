bd108238fac8c40b44dee22e87b781719905ff0f
Luciano Arnaldo Romero Calla <luccianorc@gmail.com>
2021-02-25 21:43:02 +0100

    hashcode qualification: qualification traffic weights solution, ^ BCEF

--- hashcode/2021/qualification/qualification.cpp
+++ hashcode/2021/qualification/qualification.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <algorithm>
 
 #define NS 100000
 
@@ -11,7 +12,7 @@ struct street
 {
 	int u, v, L;	// u -> v
 	char name[32];
-	bool on = false;
+	int traffic = 0;						// max num of cars at some time
 
 } streets[100000];						// input: street values
 
@@ -48,28 +49,45 @@ int main()
 		}
 	}
 	
+	int time, max_path_time = 0;
+	size_t max_stops = 0;
+
 	// step 1:
 	for(int i = 0; i < V; i++)
 	{
+		time = 0;
+
 		vector<int> & vcar = cars[i];
 		for(const int & s: vcar)
 		{
 			street & st = streets[s];
-			if(!st.on)
-			{
-				st.on = true;
+			if(!st.traffic)
 				intersections[st.v].push_back(s);
-			}
+			st.traffic++;
+			time += st.L;
 		}
+
+		time -= streets[vcar.front()].L + streets[vcar.back()].L;
+		max_path_time = max(max_path_time, time);
+
+		max_stops = max(max_stops, vcar.size() - 1);
 	}
 	
-	int A = 0;
+	time = max((D - max_path_time) / max_stops, 1lu);		// max time per stop loop
+
+	fprintf(stderr, "streets: %d\n", S);
+	fprintf(stderr, "intersections: %d\n", I);
+	fprintf(stderr, "cars: %d\n", V);
+	fprintf(stderr, "D: %10d, max_path_time: %10d\n", D, max_path_time);
+	fprintf(stderr, "time: %10d, max_stops: %10lu\n", time, max_stops);
+
+	int sum, A = 0;
 	for(int i = 0; i < I; i++)
 		A += intersections[i].size() > 0;
 	
 	printf("%d\n", A);
 
-	// naive solution
+	// adding traffic solution
 	for(int i = 0; i < I; i++)
 	{
 		vector<int> & vint = intersections[i];
@@ -78,8 +96,17 @@ int main()
 			printf("%d\n", i);
 			printf("%lu\n", vint.size());
 
+			sort(vint.begin(), vint.end(), [](const int & i, const int & j)
+			{
+				return streets[i].traffic > streets[j].traffic;
+			});
+
+			sum = 0;
 			for(const int & s: vint)
-				printf("%s %d\n", streets[s].name, 1);
+				sum += streets[s].traffic;
+
+			for(const int & s: vint)
+				printf("%s %d\n", streets[s].name, max(streets[s].traffic * time / sum, 1));
 		}
 	}
 
