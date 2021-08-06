f710110c73741bc4bad803c447acd002ee432648
Luciano Arnaldo Romero Calla <luccianorc@gmail.com>
2021-08-06 14:18:04 +0200

    uva: 11854 - Egypt (triangles)

---
+++ uva/11854.cpp
@@ -0,0 +1,18 @@
+// 11854 - Egypt
+
+#include <cstdio>
+#include <algorithm>
+
+int main()
+{
+	int a, b, c;
+	while(scanf("%d %d %d", &a, &b, &c) && a)
+	{
+		if(c < a) std::swap(c, a);
+		if(c < b) std::swap(c, b);
+		printf("%s\n", a * a + b * b == c * c ? "right" : "wrong");
+	}
+
+	return 0;
+}
+
