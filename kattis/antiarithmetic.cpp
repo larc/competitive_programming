9e06e2d50ebffc0a0838d2624a725878231da435
Luciano Arnaldo Romero Calla <luccianorc@gmail.com>
2022-10-22 21:53:16 +0200

    add uva and kattis 10730 antiarithmetic

---
+++ kattis/antiarithmetic.cpp
@@ -0,0 +1,36 @@
+#include <cstdio>
+
+#define N 10000
+
+int S[N], P[N];
+
+bool antiarithmetic(const int & n)
+{
+	for(int i = 0; i < n; ++i)
+	for(int j = i + 1; j < n; ++j)
+	{
+		const int & k = 2 * S[i] - S[j];
+		if(k < n && k >= 0 && P[k] < i)
+			return false;
+	}
+
+	return true;
+}
+
+int main()
+{
+	int n;
+	while(scanf("%d:", &n), n)
+	{
+		for(int i = 0; i < n; ++i)
+		{
+			scanf("%d", S + i);
+			P[S[i]] = i;
+		}
+
+		printf("%s\n", antiarithmetic(n) ? "yes" : "no");
+	}
+
+	return 0;
+}
+
