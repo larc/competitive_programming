6fa36bfd7fde952b8c5148ad339defa51958e3e4
Luciano Arnaldo Romero Calla <luccianorc@gmail.com>
2022-10-30 01:32:05 +0200

    add uva kattis 10714 ants

---
+++ uva/10714.cpp
@@ -0,0 +1,28 @@
+// 10714 - Ants
+
+#include <cstdio>
+#include <algorithm>
+
+int main()
+{
+	int n_cases, length, n, a;
+	int earliest, latest;
+
+	scanf("%d", &n_cases);
+	while(n_cases--)
+	{
+		earliest = latest = 0;
+
+		scanf("%d %d", &length, &n);
+		while(n--)
+		{
+			scanf("%d", &a);
+			earliest = std::max(earliest, std::min(a, length - a));
+			latest = std::max(latest, std::max(a, length - a));
+		}
+		printf("%d %d\n", earliest, latest);
+	}
+
+	return 0;
+}
+
