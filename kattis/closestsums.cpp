ffa3e965398fef1007aab26901c311aa5916eed5
Luciano Arnaldo Romero Calla <luccianorc@gmail.com>
2022-10-29 22:49:33 +0200

    update uva kattis 10487 closestsums
    a better solution to the two old ones

--- kattis/closestsums.cpp
+++ kattis/closestsums.cpp
@@ -1,42 +1,49 @@
 #include <cstdio>
-#include <vector>
 #include <algorithm>
 
-std::vector<int> S;
-const int & closest_sum(const int & q)
+int closest_sum(const int * A, const int & n, const int & q)
 {
-	int i = std::lower_bound(S.begin(), S.end(), q) - S.begin();
-	if(i == S.size()) return S.back();
-	if(i == 0) return S.front();
-	return q - S[i - 1] < S[i] - q ? S[i - 1] : S[i];
+	const int & i = std::lower_bound(A, A + n, q) - A;
+	if(i == n) return n - 1;
+	if(i == 0) return 0;
+	return i;
 }
 
 int main()
 {
-	int A[1000], n, q, n_case = 0;
-	S.reserve(1000000);
+	int A[1000];
+	int m, n, q, closest, n_case = 0;
 
-	while(scanf("%d", &n) != EOF)
+	auto update_closest = [&closest, &q](const int & sum)
+	{
+		if(std::abs(q - sum) < std::abs(q - closest))
+			closest = sum;
+	};
+
+	while(scanf("%d", &n) != EOF && n)
 	{
 		for(int i = 0; i < n; ++i)
 			scanf("%d", A + i);
 
-		for(int i = 0; i < n; ++i)
-		for(int j = i + 1; j < n; ++j)
-			S.push_back(A[i] + A[j]);
-
-		std::sort(S.begin(), S.end());
+		std::sort(A, A + n);
 
 		printf("Case %d:\n", ++n_case);
 
-		scanf("%d", &n);
-		while(n--)
+		scanf("%d", &m);
+		while(m--)
 		{
 			scanf("%d", &q);
-			printf("Closest sum to %d is %d.\n", q, closest_sum(q));
-		}
+			closest = 100000000;
+			for(int i = 0; i < n; ++i)
+			{
+				const int & j = closest_sum(A, n, q - A[i]);
+				if(j != i	) update_closest(A[i] + A[j]);
+				if(j > 0	) update_closest(A[i] + A[j - 1]);
+				if(j < n + 1) update_closest(A[i] + A[j + 1]);
+			}
 
-		S.clear();
+			printf("Closest sum to %d is %d.\n", q, closest);
+		}
 	}
 
 	return 0;
--- uva/10487.cpp
+++ uva/10487.cpp
@@ -1,33 +1,50 @@
 // 10487 - Closest Sums
 
 #include <cstdio>
-#include <cmath>
+#include <algorithm>
 
-#define N 1000
-
-int A[N];
+int closest_sum(const int * A, const int & n, const int & q)
+{
+	const int & i = std::lower_bound(A, A + n, q) - A;
+	if(i == n) return n - 1;
+	if(i == 0) return 0;
+	return i;
+}
 
 int main()
 {
-	int m, n, q, sum, ncase = 0;
-	while(scanf("%d", &n), n)
+	int A[1000];
+	int m, n, q, closest, n_case = 0;
+
+	auto update_closest = [&closest, &q](const int & sum)
+	{
+		if(std::abs(q - sum) < std::abs(q - closest))
+			closest = sum;
+	};
+
+	while(scanf("%d", &n) != EOF && n)
 	{
 		for(int i = 0; i < n; ++i)
 			scanf("%d", A + i);
 
-		printf("Case %d:\n", ++ncase);
+		std::sort(A, A + n);
+
+		printf("Case %d:\n", ++n_case);
 
 		scanf("%d", &m);
 		while(m--)
 		{
 			scanf("%d", &q);
-			sum = A[0] + A[1];
+			closest = 100000000;
 			for(int i = 0; i < n; ++i)
-			for(int j = 0; j < n; ++j)
-				if(A[i] != A[j] && std::abs(A[i] + A[j] - q) < std::abs(sum - q))
-					sum = A[i] + A[j];
+			{
+				const int & j = closest_sum(A, n, q - A[i]);
+				if(j != i	) update_closest(A[i] + A[j]);
+				if(j > 0	) update_closest(A[i] + A[j - 1]);
+				if(j < n + 1) update_closest(A[i] + A[j + 1]);
+			}
 
-			printf("Closest sum to %d is %d.\n", q, sum);
+			printf("Closest sum to %d is %d.\n", q, closest);
 		}
 	}
 
