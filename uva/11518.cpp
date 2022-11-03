64ac7d123618898eac770ef878bc4117adf7b241
Luciano Arnaldo Romero Calla <luccianorc@gmail.com>
2022-11-03 23:25:17 +0100

    add uva kattis 11518 dominoes2

---
+++ uva/11518.cpp
@@ -0,0 +1,57 @@
+// 11518 - Dominos 2
+
+#include <cstdio>
+#include <vector>
+
+#define N 10001
+
+std::vector<int> G[N];
+bool visited[N];
+
+int count_dfs(const int & u)
+{
+	int count = 0;
+
+	visited[u] = true;
+	for(const int & v: G[u])
+		if(!visited[v])
+			count += count_dfs(v);
+
+	return count + 1;
+}
+
+int main()
+{
+	int n_cases, n, m, l, x, y;
+
+	scanf("%d", &n_cases);
+	while(n_cases--)
+	{
+		scanf("%d %d %d", &n, &m, &l);
+
+		for(int i = 1; i <= n; ++i)
+		{
+			G[i].clear();
+			visited[i] = false;
+		}
+
+		while(m--)
+		{
+			scanf("%d %d", &x, &y);
+			G[x].push_back(y);
+		}
+
+		m = 0;
+		while(l--)
+		{
+			scanf("%d", &x);
+			if(!visited[x])
+				m += count_dfs(x);
+		}
+
+		printf("%d\n", m);
+	}
+
+	return 0;
+}
+
