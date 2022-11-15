e560c3634331573005f5eb71aeed83da134ffc64
Luciano Arnaldo Romero Calla <luccianorc@gmail.com>
2022-11-15 23:36:47 +0100

    add uva kattis 1738 ceiling

---
+++ uva/1738.cpp
@@ -0,0 +1,105 @@
+// 1738 - Ceiling Function
+
+#include <cstdio>
+#include <vector>
+
+struct bst
+{
+	struct node
+	{
+		int value;
+		node * child[2] = {};
+
+		node(const int & val): value(val) {}
+		~node()
+		{
+			delete child[0];
+			delete child[1];
+		}
+	};
+
+	node * root = nullptr;
+
+	~bst()
+	{
+		delete root;
+	}
+
+	bool find(node **& n, const int & val)
+	{
+		n = &root;
+		while(*n)
+		{
+			if((*n)->value == val)
+				return true;
+
+			n = &(*n)->child[(*n)->value < val];
+		}
+
+		return false;
+	}
+
+	bool insert(const int & val)
+	{
+		node ** n;
+		if(find(n, val))
+			return false;
+
+		*n = new node(val);
+		return true;
+	}
+
+	bool operator == (const bst & t)
+	{
+		return equal(root, t.root);
+	}
+
+	bool equal(const node * a, const node * b) const
+	{
+		if(a && b)
+			return equal(a->child[0], b->child[0]) && equal(a->child[1], b->child[1]);
+
+		return !a && !b;
+	}
+};
+
+int main()
+{
+	int n, k, v, count;
+	bool repeat;
+
+	std::vector<bst> vbst;
+	vbst.reserve(50);
+
+	while(scanf("%d %d", &n, &k) != EOF)
+	{
+		vbst.clear();
+		vbst.resize(n);
+
+		count = 0;
+		for(int i = 0; i < n; ++i)
+		{
+			bst & t = vbst[i];
+			for(int j = 0; j < k; ++j)
+			{
+				scanf("%d", &v);
+				t.insert(v);
+			}
+
+			repeat = false;
+			for(int j = 0; j < i; ++j)
+				if(t == vbst[j])
+				{
+					repeat = true;
+					break;
+				}
+
+			if(!repeat) ++count;
+		}
+
+		printf("%d\n", count);
+	}
+
+	return 0;
+}
+
