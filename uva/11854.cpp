5c2e9f225af20f9f7877560e9c703b4dc62c8aa5
Luciano Arnaldo Romero Calla <luccianorc@gmail.com>
2024-03-19 00:40:18 +0100

    re added 11854.cpp

--- uva/11854.cpp
+++ uva/11854.cpp
@@ -1,58 +1,16 @@
-// 11503 - Virtual Friends
+// 11854 - Egypt
 
 #include <cstdio>
-#include <string>
-#include <unordered_map>
-
-#define N 200001
-
-using namespace std;
-
-int num[N];
-int friends[N];
-
-int find_f(const int & x)
-{
-	return x == friends[x] ? x : friends[x] = find_f(friends[x]);
-}
-
-int union_f(int & x, int & y)
-{
-	x = find_f(x);
-	y = find_f(y);
-
-	if(x != y)
-	{
-		num[x] += num[y];
-		friends[y] = x;
-	}
-
-	return num[x];
-}
+#include <algorithm>
 
 int main()
 {
-	int c, n, id;
-	unordered_map<string, int> ids;
-	char name_a[21], name_b[21];
-
-	scanf("%d", &c);
-	while(c--)
+	int a, b, c;
+	while(scanf("%d %d %d", &a, &b, &c) && a)
 	{
-		ids.clear();
-		id = 0;
-
-		scanf("%d", &n);
-		while(n--)
-		{
-			scanf("%s %s", name_a, name_b);
-			int & a = ids[name_a];
-			int & b = ids[name_b];
-			if(!a) { a = ++id; num[a] = 1; friends[a] = a; }
-			if(!b) { b = ++id; num[b] = 1; friends[b] = b; }
-
-			printf("%d\n", union_f(a, b));
-		}
+		if(c < a) std::swap(c, a);
+		if(c < b) std::swap(c, b);
+		printf("%s\n", a * a + b * b == c * c ? "right" : "wrong");
 	}
 
 	return 0;
