b682f6efe43bd5f87ab3cbb72b672f5cb5e6d0bb
Luciano Arnaldo Romero Calla <luccianorc@gmail.com>
2024-09-16 16:48:21 +0200

    beecrowd 3214 - Soda Surpler

---
+++ beecrowd/3214.cpp
@@ -0,0 +1,26 @@
+// 3214 - Soda Surpler
+
+#include <cstdio>
+
+
+int main()
+{
+	int e, f, c, ans;
+
+	while(scanf("%d %d %d", &e, &f, &c) != EOF)
+	{
+		f += e;
+		ans = 0;
+		while(f >= c)
+		{
+			e = f / c;
+			f = f % c + e;
+			ans += e;
+		}
+
+		printf("%d\n", ans);
+	}
+
+	return 0;
+}
+
