08ed5a0dfa1ab86d25cd9c444939d4c98e234268
Luciano Arnaldo Romero Calla <luccianorc@gmail.com>
2024-09-16 19:49:26 +0200

    kattis eventplanning

---
+++ kattis/eventplanning.cpp
@@ -0,0 +1,30 @@
+#include <cstdio>
+
+#define INF 1E9
+
+int main()
+{
+	int n, b, h, w, min, p, i, a, tmp;
+
+	while(scanf("%d %d %d %d", &n, &b, &h, &w) != EOF)
+	{
+		min = INF;
+		while(h--)
+		{
+			scanf("%d", &p);
+			for(i = 0; i < w; ++i)
+			{
+				scanf("%d", &a);
+				if(a >= n)
+				{
+					tmp = p * n;
+					if(tmp < min) min = tmp;
+				}
+			}
+		}
+
+		min <= b ? printf("%d\n", min) : printf("stay home\n");
+	}
+
+	return 0;
+}
