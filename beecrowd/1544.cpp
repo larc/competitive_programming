06c9a24f27ee4c73da0c88588eb596432f4dfbc0
Luciano Arnaldo Romero Calla <luccianorc@gmail.com>
2024-09-17 10:34:01 +0200

    beecrowd

---
+++ beecrowd/1544.cpp
@@ -0,0 +1,27 @@
+// 1544 - The Last Non-zero Digit
+
+#include <cstdio>
+#include <cstring>
+
+int main()
+{
+	long long n, m, p;
+
+	while(scanf("%lld %lld", &n, &m) != EOF)
+	{
+		p = 1;
+
+		m = n - m;
+		while(n > m)
+		{
+			p *= n--;
+			while(p % 10 == 0) p /= 10;
+			p %= 1000000000;
+		}
+
+		printf("%lld\n", p % 10);
+	}
+
+	return 0;
+}
+
