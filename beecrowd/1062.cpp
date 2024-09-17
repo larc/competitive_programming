c284676a7cde01d59aaf27a4de05801e78b30b5f
Luciano Arnaldo Romero Calla <luccianorc@gmail.com>
2024-09-17 09:20:17 +0200

    beecrowd  1062 - Rails

---
+++ beecrowd/1062.cpp
@@ -0,0 +1,42 @@
+// 1062 - Rails
+
+#include <cstdio>
+#include <stack>
+
+#define N 1001
+
+using namespace std;
+
+int main()
+{
+	int order[N];
+	int n, c;
+
+	while(scanf("%d", &n), n)
+	{
+		while(scanf("%d", &order[1]), order[1])
+		{
+			for(int i = 2; i <= n; ++i)
+				scanf("%d", &order[i]);
+
+			c = 1;
+			stack<int> s;
+			for(int i = 1; i <= n; ++i)
+			{
+				while((s.empty() || s.top() != order[i]) && c <= n)
+					s.push(c++);
+
+				if(!s.empty() && s.top() == order[i])
+					s.pop();
+			}
+
+			if(s.empty()) printf("Yes\n");
+			else printf("No\n");
+		}
+
+		printf("\n");
+	}
+
+	return 0;
+}
+
