01fab59681e984ed9a5e73973d9a8aca3978c9dc
Luciano Arnaldo Romero Calla <luccianorc@gmail.com>
2024-09-17 10:10:09 +0200

    beecrowd 1398 - Ocean Deep! Make it Shallow!!

---
+++ beecrowd/1398.cpp
@@ -0,0 +1,34 @@
+// 1398 - Ocean Deep! Make it Shallow!!
+
+#include <cstdio>
+
+int main()
+{
+	char c[128],  * i;
+	int n;
+
+	while(scanf("%s", c) != EOF)
+	{
+		i = c;
+		n = 0;
+		while(*i)
+		{
+			if(*i == '#')
+			{
+				if(n) printf("NO\n");
+				else printf("YES\n");
+				n = 0;
+			}
+			else
+			{
+				n <<= 1;
+				n += *i - '0';
+				n %= 131071;
+			}
+			++i;
+		}
+	}
+
+	return 0;
+}
+
