ef98d046bf8d8618e223ba79c75178b1f35bb7cd
Luciano Arnaldo Romero Calla <luccianorc@gmail.com>
2024-09-17 10:54:10 +0200

    beecrowd 1091 - Division of Nlogonia

---
+++ beecrowd/1091.cpp
@@ -0,0 +1,26 @@
+// 1091 - Division of Nlogonia
+
+#include <cstdio>
+
+int main()
+{
+	int k, n, m, x, y;
+
+	while(scanf("%d", &k) != EOF && k)
+	{
+		scanf("%d %d", &n, &m);
+		while(k--)
+		{
+			scanf("%d %d", &x, &y);
+			if(x == n) printf("divisa\n");
+			else if(y == m) printf("divisa\n");
+			else if(x > n && y > m) printf("NE\n");
+			else if(x > n && y < m) printf("SE\n");
+			else if(x < n && y < m) printf("SO\n");
+			else printf("NO\n");
+		}
+	}
+
+	return 0;
+}
+
